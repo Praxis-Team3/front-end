fe9665174eb44684a4b7317937e9b87bf29fdf26
Miguel Pinzon Hernandez <cmpinzonh@unal.edu.co>
2018-09-24 11:45:39 -0500

    Added a survey model and binded it to the survey component

--- team3front/src/app/surveys/surveys.component.ts
+++ team3front/src/app/surveys/surveys.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Survey } from '../models/survey';
 
 export interface Tutor {
   name: string;
@@ -13,15 +14,24 @@ export interface Tutor {
 export class SurveysComponent implements OnInit {
 
   constructor() { }
-
+  sessionDate: Date;
+  survey = new Survey(this.sessionDate,'','','','','','','','','','','','');
   sessionType: string;
   tutorName: string;
+  
+  satisfactionLevel: string;
+  knowledgeTopic: string;
 
   ngOnInit() {
   }
 
   send(): void{
-
+    console.log(this.survey.sessionType);
+    console.log(this.survey.sessionDate);  
+    console.log(this.survey.tutorName);  
+    console.log(this.survey.satisfactionLevel);  
+    console.log(this.survey.knowledgeTopic);  
+    console.log(this.survey.sessionResources);  
   }
   tutors: Tutor[] = [
     {name: 'David', subject: 'Introduction'},
